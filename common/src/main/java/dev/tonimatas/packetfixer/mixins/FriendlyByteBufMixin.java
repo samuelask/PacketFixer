1e35eaeb51c3db9f90b0bd034d99b22ab1965be5
TonimatasDEV <amatascubero@gmail.com>
2024-04-20 19:29:49 +0200

    Fix #35

---
+++ common/src/main/java/dev/tonimatas/packetfixer/mixins/FriendlyByteBufMixin.java
@@ -0,0 +1,14 @@
+package dev.tonimatas.packetfixer.mixins;
+
+import net.minecraft.network.FriendlyByteBuf;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(value = FriendlyByteBuf.class, priority = 9999)
+public abstract class FriendlyByteBufMixin {
+    @ModifyConstant(method = "readNbt()Lnet/minecraft/nbt/CompoundTag;", constant = @Constant(longValue = 2097152L))
+    public long newSize(long constant) {
+        return Long.MAX_VALUE;
+    }
+}
