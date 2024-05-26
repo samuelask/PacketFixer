27e692526c03602f1d520e58afaa4bdfd7b301a2
TonimatasDEV <amatascubero@gmail.com>
2024-05-26 12:56:22 +0200

    Add config and delete useless things

--- common/src/main/java/dev/tonimatas/packetfixer/mixins/FriendlyByteBufMixin.java
+++ common/src/main/java/dev/tonimatas/packetfixer/mixins/FriendlyByteBufMixin.java
@@ -1,5 +1,6 @@
 package dev.tonimatas.packetfixer.mixins;
 
+import dev.tonimatas.packetfixer.util.Config;
 import net.minecraft.network.FriendlyByteBuf;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
@@ -9,6 +10,6 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 public abstract class FriendlyByteBufMixin {
     @ModifyConstant(method = "readNbt()Lnet/minecraft/nbt/CompoundTag;", constant = @Constant(longValue = 2097152L))
     public long newSize(long constant) {
-        return Long.MAX_VALUE;
+        return Config.getNbtMaxSize();
     }
 }
