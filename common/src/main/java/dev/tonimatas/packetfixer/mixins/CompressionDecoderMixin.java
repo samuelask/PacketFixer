a627d38d2384bee3fc25aac992375109fb9b7f0c
tonimatasdev <me@tonimatas.dev>
2025-02-11 21:02:48 +0100

    Version 2.0.0

--- common/src/main/java/dev/tonimatas/packetfixer/mixins/CompressionDecoderMixin.java
+++ common/src/main/java/dev/tonimatas/packetfixer/mixins/CompressionDecoderMixin.java
@@ -3,10 +3,9 @@ package dev.tonimatas.packetfixer.mixins;
 import dev.tonimatas.packetfixer.util.Config;
 import net.minecraft.network.CompressionDecoder;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.*;
 
-@Mixin(value = CompressionDecoder.class, priority = 9999)
+@Mixin(CompressionDecoder.class)
 public class CompressionDecoderMixin {
     @ModifyConstant(method = "decode", constant = @Constant(intValue = 8388608))
     private int newSize(int value) {
